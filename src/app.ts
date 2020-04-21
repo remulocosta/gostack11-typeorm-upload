198d7580885dd446977bb125815bd5aaba226f0f
Remulo Costa <remulo.costa@gmail.com>
2020-04-21 13:41:53 -0300

    created migrations transactions, categories

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,9 @@ import 'dotenv/config';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
-import routes from './routes';
-import AppError from './errors/AppError';
-
 import createConnection from './database';
+import AppError from './errors/AppError';
+import routes from './routes';
 
 createConnection();
 const app = express();
